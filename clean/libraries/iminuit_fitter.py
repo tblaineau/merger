d64e973a0dbfb650db69de4e0c4f8308f6c02049
tristan blaineau <tristan.blaineau@orange.fr>
2019-02-27 10:20:26 +0100

    added fitter to clean, modified merger.py to fit

---
+++ clean/libraries/iminuit_fitter.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+from iminuit import Minuit
+import time
+import matplotlib.pyplot as plt
+
+GLOBAL_COUNTER=0
+
+def microlensing_event(t, u0, t0, tE, mag1):
+	u = np.sqrt(u0*u0 + ((t-t0)**2)/tE/tE)
+	return -2.5*np.log10((u**2+2)/(u*np.sqrt(u**2+4)))+mag1 
+
+def fit_ml(subdf):
+
+	#sélection des données, pas plus de 10% du temps de calcul en moyenne (0.01s vs 0.1s)
+	#le fit peut durer jusqu'à 0.7s ou aussi rapide que 0.04s (en général False)
+	maskRE = subdf.red_E.notnull() & subdf.rederr_E.notnull()
+	maskBE = subdf.blue_E.notnull() & subdf.blueerr_E.notnull()
+	maskRM = subdf.red_M.notnull() & subdf.rederr_M.notnull()
+	maskBM = subdf.blue_M.notnull() & subdf.blueerr_M.notnull()
+
+
+
+	errRE = subdf[maskRE].rederr_E.values
+	errBE = subdf[maskBE].blueerr_E.values
+	errRM = subdf[maskRM].rederr_M.values
+	errBM = subdf[maskBM].blueerr_M.values
+
+	min_err = 0.01
+	cre = (errRE>min_err) & (errRE<9.999)
+	cbe = (errBE>min_err) & (errBE<9.999)
+	crm = (errRM>min_err) & (errRM<9.999)
+	cbm = (errBM>min_err) & (errBM<9.999)
+
+	# magRE = subdf[maskRE][cre].ampli_red_E.values
+	# magBE = subdf[maskBE][cbe].ampli_blue_E.values
+	# magRM = subdf[maskRM][crm].ampli_red_M.values
+	# magBM = subdf[maskBM][cbm].ampli_blue_M.values
+
+	magRE = subdf[maskRE][cre].red_E.values
+	magBE = subdf[maskBE][cbe].blue_E.values
+	magRM = subdf[maskRM][crm].red_M.values
+	magBM = subdf[maskBM][cbm].blue_M.values
+
+	timeRE = subdf[maskRE][cre].time.values
+	timeBE = subdf[maskBE][cbe].time.values
+	timeRM = subdf[maskRM][crm].time.values
+	timeBM = subdf[maskBM][cbm].time.values
+
+	errRE = errRE[cre]
+	errBE = errBE[cbe]
+	errRM = errRM[crm]
+	errBM = errBM[cbm]
+
+	def least_squares_microlens(u0, t0, tE, magStarRE, magStarBE, magStarRM, magStarBM):
+		lsq1 = np.sum(((magRE - microlensing_event(timeRE, u0, t0, tE, magStarRE))/ errRE)**2)
+		lsq2 = np.sum(((magBE - microlensing_event(timeBE, u0, t0, tE, magStarBE))/ errBE)**2)
+		lsq3 = np.sum(((magRM - microlensing_event(timeRM, u0, t0, tE, magStarRM))/ errRM)**2)
+		lsq4 = np.sum(((magBM - microlensing_event(timeBM, u0, t0, tE, magStarBM))/ errBM)**2)
+		return lsq1+lsq2+lsq3+lsq4
+
+	def least_squares_flat(f_magStarRE, f_magStarBE, f_magStarRM, f_magStarBM):
+		return np.sum(((magRE - f_magStarRE)/errRE)**2) + np.sum(((magRM - f_magStarRM)/errRM)**2) + np.sum(((magBE - f_magStarBE)/errBE)**2) + np.sum(((magBM - f_magStarBM)/errBM)**2)
+
+	m_micro = Minuit(least_squares_microlens, 
+		u0=0.5, 
+		t0=50000, 
+		tE=1000, 
+		magStarRE=20, 
+		magStarBE=20, 
+		magStarRM=-4, 
+		magStarBM=-4, 
+		error_u0=0.1, 
+		error_t0=5000, 
+		error_tE=100, 
+		error_magStarRE=2, 
+		error_magStarBE=2., 
+		error_magStarRM=2., 
+		error_magStarBM=2., 
+		limit_u0=(0,2), 
+		limit_tE=(300, 10000),
+		limit_t0=(40000, 60000),#(48927, 52698)
+		errordef=1,
+		print_level=0)
+
+	m_flat = Minuit(least_squares_flat, 
+		f_magStarRE=20, 
+		f_magStarBE=20, 
+		f_magStarRM=-4, 
+		f_magStarBM=-4, 
+		error_f_magStarRE=2, 
+		error_f_magStarBE=2., 
+		error_f_magStarRM=2., 
+		error_f_magStarBM=2., 
+		errordef=1,
+		print_level=0
+		)
+
+	m_micro.migrad()
+	m_flat.migrad()
+	global GLOBAL_COUNTER
+	GLOBAL_COUNTER+=1
+	print(str(GLOBAL_COUNTER)+" : "+subdf.id_M.iloc[0]+" "+str(m_micro.get_fmin().is_valid)+"     ", end='\r')
+	return pd.Series(
+
+		m_micro.values.values()+[m_micro.get_fmin().is_valid, m_micro.fval] 
+		+ 
+		m_flat.values.values()+[m_flat.get_fmin().is_valid, m_flat.fval]
+		+[len(errRE)+len(errBE)+len(errRM)+len(errBM)], 
+
+		index=m_micro.values.keys()+['micro_valid', 'micro_fval']
+		+
+		m_flat.values.keys()+['flat_valid', 'flat_fval']
+		+["dof"]
+		)
+
+def fit_all(filename):
+	"""Fit all curves in filename
+	
+	[description]
+	
+	Arguments:
+		filename {str} -- Name of the file containing the merged curves.
+	"""
+	WORKING_DIR_PATH = "/Volumes/DisqueSauvegarde/working_dir/"
+	if filename[-4:]!='.pkl':
+		filename+='.pkl'
+	print("Loading "+filename)
+	merged = pd.read_pickle(WORKING_DIR_PATH+filename)
+	merged.replace(to_replace=[99.999,-99.], value=np.nan, inplace=True)
+	merged.dropna(axis=0, how='all', subset=['blue_E', 'red_E', 'blue_M', 'red_M'], inplace=True)
+	print("FILES LOADED")
+	start = time.time()
+	res= merged.groupby("id_E").apply(fit_ml)
+	end= time.time()
+	res.to_pickle(WORKING_DIR_PATH+'res_'+filename+'.pkl')
+	print(str(end-start)+" seconds elapsed.")
+	print(str(len(res))+" stars fitted.")
--- clean/merger.py
+++ clean/merger.py
@@ -13,7 +13,7 @@ import os
 
 import sys
 sys.path.append('libraries')
-import merger_library
+import merger_library, iminuit_fitter
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -21,6 +21,7 @@ if __name__ == '__main__':
 	parser.add_argument('--EROS-CCD', '-ccdE', type=int, default=None, choices=np.arange(7), required=False)
 	parser.add_argument('--MACHO-field', '-fM', type=int, required=True)
 	parser.add_argument('--output-directory', '-odir', type=str, default=merger_library.WORKING_DIR_PATH)
+	parser.add_argument('-fit', action='store_true')
 
 	args = parser.parse_args()
 
@@ -32,11 +33,18 @@ if __name__ == '__main__':
 	MACHO_field = args.MACHO_field
 	EROS_field = args.EROS_field
 	EROS_CCD = args.EROS_CCD
+	fit = args.fit
+
+	print(fit)
 
 	if EROS_CCD:
 		eros_ccd = "lm"+EROS_field+str(EROS_CCD)
 		merger_library.merger(working_dir_path, MACHO_field, eros_ccd)
+		if fit:
+			iminuit_fitter.fit_all(str(MACHO_field)+"_"+str(eros_ccd)+".pkl")
 	else:
 		for i in range(0,8):
 			eros_ccd = "lm"+EROS_field+str(i)
 			merger_library.merger(working_dir_path, MACHO_field, eros_ccd)
+			if fit:
+				iminuit_fitter.fit_all(str(MACHO_field)+"_"+str(eros_ccd)+".pkl")
