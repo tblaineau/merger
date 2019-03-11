4fee62ad6cba8a19c4994625bb5ac08beacc8e7f
tristan blaineau <tristan.blaineau@orange.fr>
2019-03-11 16:58:41 +0100

    modified parallax to be real

--- utils/toy_simulator.py
+++ utils/toy_simulator.py
@@ -10,24 +10,43 @@ def microlensing_amplification(t, u0, t0, tE):
 # def microlens(t, mag, u0, t0, tE):
 # 	return mag - 2.5*np.log10(microlensing_amplification(t, u0, t0, tE))
 
-def parallax(t, mag, u0, t0, tE, delta_u, beta, psi, alpha0):
-	phi = 2*np.pi/365.25 * (t-t0) + alpha0
-	t1 = u0*u0
-	t2 = ((t-t0)/tE)**2
-	t3 = delta_u**2 * (np.cos(phi)**2 + (np.sin(phi) * np.cos(beta))**2)
-	t4 = -delta_u * (t-t0)/tE * (np.cos(psi) * np.cos(phi) + np.cos(beta) * np.sin(psi) * np.sin(phi))
-	t5 = u0 * delta_u * (np.sin(psi) * np.cos(phi) - np.cos(psi) * np.sin(phi) * np.cos(beta))
-	u = np.sqrt(t1+t2+t3+t4+t5)
+PERIOD_EARTH = 365.2422
+
+def parallax(t, mag, u0, t0, tE, delta_u, theta):
+	# beta = delta - np.pi/2
+	alphaS = 80.8941667*np.pi/180.
+	deltaS = -69.7561111*np.pi/180.
+	epsilon = (90. - 66.56070833)*np.pi/180.
+	t_origin = 58747 #(21 septembre 2019)
+	sin_beta = np.cos(epsilon)*np.sin(deltaS) - np.sin(epsilon)*np.cos(deltaS)*np.sin(alphaS)
+	xsi = (t-t0)/tE
+	phi = 2*np.pi * (t-t_origin)/PERIOD_EARTH - alphaS
+	u_D = np.array([ 
+		-u0*np.sin(theta) + xsi*np.cos(theta),
+		 u0*np.cos(theta) + xsi*np.sin(theta)
+		])
+	u_t = np.array([
+		-delta_u*np.sin(phi),
+		 delta_u*np.cos(phi)*sin_beta
+		])
+	# t1 = u0*u0
+	# t2 = ((t-t0)/tE)**2
+	# t3 = delta_u**2 * (np.cos(phi)**2 + (np.sin(phi) * np.cos(beta))**2)
+	# t4 = -delta_u * (t-t0)/tE * (np.cos(theta) * np.cos(phi) + np.cos(beta) * np.sin(theta) * np.sin(phi))
+	# t5 = u0 * delta_u * (np.sin(theta) * np.cos(phi) - np.cos(theta) * np.sin(phi) * np.cos(beta))
+	# u = np.sqrt(t1+t2+t3+t4+t5)
+	u = np.linalg.norm(u_D-u_t, axis=0)
 	return (u**2+2)/(u*np.sqrt(u**2+4))
 
-def microlens(t, mag, blend, u0, t0, tE, delta_u, beta, psi, alpha0):
-	return - 2.5*np.log10(blend*np.power(10, mag/-2.5) + (1-blend)*np.power(10, mag/-2.5) * parallax(t, mag, u0, t0, tE, delta_u, beta, psi, alpha0))
+def microlens(t, mag, blend, u0, t0, tE, delta_u, theta):
+	return - 2.5*np.log10(blend*np.power(10, mag/-2.5) + (1-blend)*np.power(10, mag/-2.5) * parallax(t, mag, u0, t0, tE, delta_u, theta))
 
-def projected_plan(t, mag, blend, u0, t0, tE, delta_u, beta, psi, alpha0):
-	xD = (t-t0)/tE * np.cos(psi) - u0*np.sin(psi)
-	yD = (t-t0)/tE * np.sin(psi) + u0*np.cos(psi)
+def projected_plan(t, mag, blend, u0, t0, tE, delta_u, theta):
+	alpha0 = 2*np.pi/PERIOD_EARTH*t0%PERIOD_EARTH
+	xD = (t-t0)/tE * np.cos(theta) - u0*np.sin(theta)
+	yD = (t-t0)/tE * np.sin(theta) + u0*np.cos(theta)
 
-	phi = 2*np.pi/365.25 * (t-t0) + alpha0
+	phi = 2*np.pi/PERIOD_EARTH * (t-t0) + alpha0
 	x = delta_u * np.cos(phi)
 	y = delta_u * np.sin(phi) * np.cos(beta)
 	return xD, yD, x, y
@@ -49,17 +68,15 @@ params = {
 	't0':t0,
 	'tE':tE,
 	'delta_u':0,	#no parallax
-	'beta':10*np.pi/180.,
-	'psi':10*np.pi/180.,
-	'alpha0':2.*np.pi/3.
+	'theta':10*np.pi/180.
 }
 
 line, = ax0.plot(time,  microlens(time, *params.values()), color='black', linewidth=0.5)
-xD, yD, xpE, ypE = projected_plan(time, *params.values())
-defl_line, = ax1.plot(xD, yD)
-earth_projected_orbit, = ax1.plot(xpE, ypE)
-ax1.add_patch(malptch.Circle((0,0),1, fill=False, color="black"))
-ax1.axis("equal")
+#xD, yD, xpE, ypE = projected_plan(time, *params.values())
+#defl_line, = ax1.plot(xD, yD)
+#earth_projected_orbit, = ax1.plot(xpE, ypE)
+#ax1.add_patch(malptch.Circle((0,0),1, fill=False, color="black"))
+#ax1.axis("equal")
 
 fig2 = plt.figure()
 
@@ -70,7 +87,7 @@ t0slider_ax = fig2.add_axes([0.25, 0.05, 0.65, 0.03])
 t0slider = Slider(t0slider_ax, 't0', 48000, 53000, valinit=t0)
 
 tEslider_ax = fig2.add_axes([0.25, 0.15, 0.65, 0.03])
-tEslider = Slider(tEslider_ax, 'tE', 100, 10000, valinit=tE)
+tEslider = Slider(tEslider_ax, 'tE', -10000, 10000, valinit=tE)
 
 magslider_ax = fig2.add_axes([0.25, 0.2, 0.65, 0.03])
 magslider = Slider(magslider_ax, 'mag', 10, 23, valinit=mag)
@@ -78,14 +95,8 @@ magslider = Slider(magslider_ax, 'mag', 10, 23, valinit=mag)
 delta_uslider_ax = fig2.add_axes([0.25, 0.25, 0.65, 0.03])
 delta_uslider = Slider(delta_uslider_ax, 'delta_u', 0, 2, valinit=0)
 
-beta_slider_ax = fig2.add_axes([0.25, 0.3, 0.65, 0.03])
-beta_slider = Slider(beta_slider_ax, 'beta', 0., np.pi, valinit=10*np.pi/180.)
-
-alpha0_slider_ax = fig2.add_axes([0.25, 0.35, 0.65, 0.03])
-alpha0_slider = Slider(alpha0_slider_ax, 'alpha0', 0., 2*np.pi, valinit=10*np.pi/180.)
-
-psi_slider_ax = fig2.add_axes([0.25, 0.4, 0.65, 0.03])
-psi_slider = Slider(psi_slider_ax, 'psi', 0., 2*np.pi, valinit=10*np.pi/180.)
+theta_slider_ax = fig2.add_axes([0.25, 0.4, 0.65, 0.03])
+theta_slider = Slider(theta_slider_ax, 'theta', 0., 2*np.pi, valinit=10*np.pi/180.)
 
 blend_slider_ax = fig2.add_axes([0.25, 0.45, 0.65, 0.03])
 blend_slider = Slider(blend_slider_ax, 'blend', 0., 1, valinit=0.)
@@ -111,16 +122,8 @@ def update_delta_u(val):
 	params["delta_u"] = val
 	update_graph()
 
-def update_beta(val):
-	params["beta"] = val
-	update_graph()
-
-def update_alpha0(val):
-	params["alpha0"] = val
-	update_graph()
-
-def update_psi(val):
-	params["psi"] = val
+def update_theta(val):
+	params["theta"] = val
 	update_graph()
 
 def update_blend(val):
@@ -130,13 +133,13 @@ def update_blend(val):
 def update_graph():
 	ydata = microlens(time, *params.values())
 	line.set_ydata(ydata)
-	ax0.set_ylim(ydata.max()+1, ydata.min()-1)
+	#ax0.set_ylim(ydata.max()+1, ydata.min()-1)
 
-	xD, yD, xpE, ypE = projected_plan(time, *params.values())
-	earth_projected_orbit.set_xdata(xpE)
-	earth_projected_orbit.set_ydata(ypE)
-	defl_line.set_xdata(xD)
-	defl_line.set_ydata(yD)
+	#xD, yD, xpE, ypE = projected_plan(time, *params.values())
+	#earth_projected_orbit.set_xdata(xpE)
+	#earth_projected_orbit.set_ydata(ypE)
+	#defl_line.set_xdata(xD)
+	#defl_line.set_ydata(yD)
 	fig.canvas.draw_idle()
 	
 u0slider.on_changed(update_u0)
@@ -144,10 +147,8 @@ t0slider.on_changed(update_t0)
 tEslider.on_changed(update_tE)
 magslider.on_changed(update_mag)
 delta_uslider.on_changed(update_delta_u)
-beta_slider.on_changed(update_beta)
-alpha0_slider.on_changed(update_alpha0)
-psi_slider.on_changed(update_psi)
+theta_slider.on_changed(update_theta)
 blend_slider.on_changed(update_blend)
-ax0.set_ylim(16, 20)
+ax0.set_ylim(10, 20)
 ax0.invert_yaxis()
 plt.show()
