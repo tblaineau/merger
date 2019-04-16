ef93904a9e815f73c10ef7479ba506b6fb059703
tristanblaineau <tristan.blaineau@orange.fr>
2019-04-16 15:33:13 +0200

    no class, corrected functions

--- old/parameter_generator.py
+++ old/parameter_generator.py
@@ -1,53 +1,67 @@
 import numpy as np
-import astropy.units as u
+import astropy.units as units
 import astropy.constants as constants
 
 import matplotlib.pyplot as plt
 
-class Microlensing_parameters_generator():
-	def __init__(self, mass, u_lim, v0):
-		self.mass = mass<<u.Msun
-		self.u_lim = u_lim
-		self.v0 = v0 << (u.km/u.s)
+MASS=1000
 
-		self.t_min = 48928 << u.d
-		self.t_max = 52697 << u.d
-		self.t_obs = self.t_max - self.t_min
+r_lmc = 55000
+r_earth = (150*1e6<<units.km).to(units.pc).value
+r_0 = np.sqrt(4*constants.G/(constants.c**2)*(r_lmc<<units.pc)).decompose([units.Msun, units.pc]).value
+pc_to_km = (units.pc.to(units.km))
+t_obs = ((52697 - 48928) << units.d).to(units.s).value
+a = 5000.  # pc
+rho_0 = 0.0079  # M_sol/pc^3
+d_sol = 8500  # pc
+l_lmc, b_lmc = 280.4652 / 180. * np.pi, -32.8884 / 180. * np.pi
+r_lmc = 55000  # pc
+cosb_lmc = np.cos(b_lmc)
+cosl_lmc = np.cos(l_lmc)
 
-		self.r_lmc = 55 << u.kpc
-		self.rho_0 = 0.0079 << (u.Msun/u.pc**3)
+A = d_sol ** 2 + a ** 2
+B = d_sol * cosb_lmc * cosl_lmc
 
-		d_sol = 8.5 << u.kpc
-		a = 5 << u.kpc
-		l_lmc, b_lmc = 280.4652 / 180. * np.pi, -32.8884 / 180. * np.pi
-		cosb_lmc = np.cos(b_lmc)
-		cosl_lmc = np.cos(l_lmc)
+def rho_halo(x):
+	return rho_0*A/((x*r_lmc)**2-2*x*r_lmc*B+A)
 
-		self.A = d_sol ** 2 + a ** 2
-		self.B = d_sol * cosb_lmc * cosl_lmc
+def r(mass=MASS):
+	R_0 = r_0*np.sqrt(mass)
+	return r_earth/R_0
 
-		self.r0 = np.sqrt(4 * constants.G / (constants.c ** 2) * self.r_lmc)
+def p_xvt(x, v_T, mass=MASS):
+	u_lim = 1
+	R_E = r_0*np.sqrt(mass*x*(1-x))
+	return rho_halo(x)/mass*r_lmc*(2*u_lim*R_E*t_obs*np.abs(v_T))
 
-	def R_E(self, x):
-		return self.r0 * np.sqrt(self.mass * x * (1-x))
+def f_vt(v_T, v0=220):
+	return (2*v_T/(v0**2))*np.exp(-v_T**2/(v0**2))
 
-	def rho_x(self, x):
-		return self.rho_0 * self.A / ((x * self.r_lmc) ** 2 - 2 * x * self.r_lmc * self.B + self.A) / self.mass
+def x_from_delta_u(delta_u, mass=MASS):
+	return r(mass)**2/(r(mass)**2+delta_u**2)
 
-	def p_xvT(self, x, v_T):
-		q = self.rho_x(x) / self.mass * self.r_lmc * (2*self.u_lim*self.R_E(x)*self.t_obs * abs(v_T<<(u.km/u.s)))
-		return q.decompose().value
+def v_T_from_tEdu(delta_u, t_E, mass=MASS):
+	R_0 = r_0*np.sqrt(mass)*pc_to_km
+	ri = r(mass)
+	return R_0/(t_E*86400) * ri*delta_u/(ri**2+delta_u**2)
 
-	def f_x(self, x):
-		return self.rho_x(x) * x**2
+def jacobian(delta_u, t_E, mass=MASS):
+	R_0 = r_0*np.sqrt(mass)*pc_to_km
+	ri = r(mass)
+	h1 = -2*ri**2*delta_u/(ri**2+delta_u**2)**2
+	sqrth2 = ri*delta_u/(ri**2+delta_u**2)
+	return -h1*sqrth2*R_0/(t_E*86400)**2
 
-	def f_vT(self, v_T):
-		return (2*v_T/(self.v0**2)) * np.exp(-v_T**2/(self.v0**2))
+def pdf_xvt(x, vt, mass=MASS):
+	return p_xvt(x, vt, mass)*rho_halo(x)/mass*x*x*f_vt(vt)
 
-g = Microlensing_parameters_generator(mass=100, u_lim=1, v0=220)
+def p_tEdu(delta_u, t_E, mass=MASS):
+	x = x_from_delta_u(delta_u, mass)
+	vt = v_T_from_tEdu(delta_u, t_E, mass)
+	return p_xvt(x, vt, mass)*rho_halo(x)/mass*x*x*f_vt(vt)*np.abs(jacobian(delta_u, t_E, mass))
 
-x = np.linspace(0,1,100)
-v_T = np.linspace(-600, 600, 100)
-res = g.p_xvT(x[:,None], v_T[None,:])
-plt.imshow(res)
+resolution = 1000
+vT = np.linspace(-600, 600, resolution)
+x = np.linspace(0, 1, resolution)
+plt.imshow(pdf_xvt(x[None, :], vT[:, None], mass=60), extent=[x[0], x[-1], vT[0], vT[-1]], aspect='auto', origin='lower', cmap='inferno')
 plt.show()
