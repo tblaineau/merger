84df4a896b7ad849efeb7b7b15f0f41ffd48406e
tristanblaineau <tristan.blaineau@orange.fr>
2019-05-09 14:30:02 +0200

    work

--- merger/parallax_estimator/similarity_estimator_display.py
+++ merger/parallax_estimator/similarity_estimator_display.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-
+import time
 from merger.old.parameter_generator import microlens_parallax, microlens_simple
 
 from iminuit import Minuit
+import scipy.optimize
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -104,10 +105,10 @@ def parameter_space(dfi, params_pt=None):
 
 #
 
-df = pd.read_pickle('temp_fittermax1000.pkl')
+df = pd.read_pickle('temp_max.pkl')
 print(df.iloc[0].distance)
-df = df.join(pd.DataFrame(df.pop('distance').to_list())[['fval', 'is_valid']])
-df.rename(columns={'fval':'distance'}, inplace=True)
+# df = df.join(pd.DataFrame(df.pop('distance').to_list())[['fval', 'is_valid']])
+# df.rename(columns={'fval':'distance'}, inplace=True)
 #df = df[df.is_valid]
 
 
@@ -120,8 +121,7 @@ df.rename(columns={'fval':'distance'}, inplace=True)
 # cutoff_list = [1, 2]
 cutoff_list = [0.01, 0.1, 1]
 df = df[df.mass == 100.]
-df.distance.hist(bins=20, range=(0, 0.1), histtype='step')
-plt.show()
+
 
 # parameter_space(df[df['mass']==10.])
 # parameter_space(df[df['mass']==100.])
@@ -142,7 +142,7 @@ fraction(df, curr_muparameter='delta_u', cutoff=cutoff_list, bins=np.linspace(0,
 
 tmin = 48928
 tmax = 52697
-p1 = df.sort_values(by='distance', ascending=False).iloc[2].to_dict()
+p1 = df.sort_values(by='distance', ascending=False).iloc[0].to_dict()
 # p1 = df.iloc[np.random.randint(0, len(df))].to_dict()
 print(p1)
 p1['blend']=0.
@@ -151,7 +151,7 @@ del p1['distance']
 del p1['mass']
 del p1['x']
 del p1['vt']
-del p1['is_valid']
+p1.pop('is_valid', None)
 t = np.linspace(tmin, tmax, 10000)
 cnopa = microlens_simple(t, **p1)
 cpara = microlens_parallax(t, **p1)
@@ -160,6 +160,7 @@ fig, axs = plt.subplots(ncols=1, nrows=2, sharex='col')
 pdif1, = axs[1].plot(t, np.abs((microlens_parallax(t, 19, 0, p1['u0'], p1['t0'], p1['tE'], p1['delta_u'],p1['theta']) - microlens_simple(t, 19., 0., p1['u0'], p1['t0'], p1['tE'], 0., 0.))))
 ppar1, = axs[0].plot(t, -(microlens_parallax(t, 19, 0, p1['u0'], p1['t0'], p1['tE'], p1['delta_u'], p1['theta'])))
 pnop1, = axs[0].plot(t, -(microlens_simple(t, 19, 0, p1['u0'], p1['t0'], p1['tE'], p1['delta_u'], p1['theta'])))
+axs[0].plot(t, -(microlens_simple(t, 19, 0, p1['u0'], p1['t0'], p1['tE'], p1['delta_u'], p1['theta'])), ls='--')
 hl = axs[1].axhline(0, color='black', linewidth=0.5)
 hl2 = axs[1].axhline(0, color='red', linewidth=0.5)
 # plt.xlim(51200, 51600)
@@ -185,7 +186,8 @@ def update_plot(u0, t0, tE, r):
 def max_fitter(t, u0, t0, tE, pu0, pt0, ptE, pdu, ptheta):
 	return -np.abs((microlens_parallax(t, 19, 0, pu0, pt0, ptE, pdu, ptheta) - microlens_simple(t, 19., 0., u0, t0, tE, 0., 0.)))
 
-def fitter_minmax(u0, t0, tE):
+def fitter_minmax(params):
+	u0, t0, tE = params
 	res = scipy.optimize.differential_evolution(max_fitter, bounds=[(p1['t0']-400, p1['t0']+400)], args=(u0, t0, tE, p1['u0'], p1['t0'], p1['tE'], p1['delta_u'], p1['theta']), disp=False, popsize=40, mutation=(0.5, 1.0), strategy='best1bin')
 	# tm = Minuit(max_fitter, t=t0, error_t=100, limit_t=(tmin, tmax) ,errordef=1, print_level=0)
 	# tm.migrad()
@@ -194,42 +196,59 @@ def fitter_minmax(u0, t0, tE):
 	else:
 		r = res.fun
 	explored_parameters.append([u0, t0, tE, r])
-	update_plot(u0, t0, tE, r)
+	# update_plot(u0, t0, tE, r)
 	return -r
 
-m = Minuit(fitter_minmax,
-		   u0=p1['u0'],
-		   t0=p1['t0'],
-		   tE=p1['tE'],
-		   error_u0=0.1,
-		   error_t0=100,
-		   error_tE=100,
-		   limit_u0=(0, 1),
-		   limit_tE=(p1['tE']*(1-np.sign(p1['tE'])*0.5), p1['tE']*(1+np.sign(p1['tE'])*0.5)),
-		   limit_t0=(p1['t0']-abs(p1['tE']), p1['t0']+abs(p1['tE'])),
-		   errordef=1,
-		   print_level=1
-		   )
+def fitter_minmax_minuit(u0, t0, tE):
+	def max_fitter(t):
+		t = np.array([t])
+		return -np.abs((microlens_parallax(t, 19, 0, p1['u0'], p1['t0'], p1['tE'], p1['delta_u'], p1['theta']) - microlens_simple(t, 19., 0., u0, t0, tE, 0., 0.)))
+	tm = Minuit(max_fitter, t=t0, error_t=100, limit_t=(tmin, tmax), errordef=1, print_level=0)
+	tm.migrad()
+	update_plot(u0, t0, tE, tm.fval)
+	return tm.fval
 
 
-def onclick(event):
-	m.migrad()
-	# res.append(scipy.optimize.dual_annealing(fitter_minmax,
-	# 		bounds=[(0, 1), (p1['t0'] - abs(p1['tE']), p1['t0'] + abs(p1['tE'])), (p1['tE'] * (1 - np.sign(p1['tE']) * 0.5), p1['tE'] * (1 + np.sign(p1['tE']) * 0.5))]))
+# m = Minuit(fitter_minmax,
+# 		   u0=p1['u0'],
+# 		   t0=p1['t0'],
+# 		   tE=p1['tE'],
+# 		   error_u0=0.2,
+# 		   error_t0=50,
+# 		   error_tE=10,
+# 		   limit_u0=(0, 1),
+# 		   limit_tE=(p1['tE']*(1-np.sign(p1['tE'])*0.5), p1['tE']*(1+np.sign(p1['tE'])*0.5)),
+# 		   limit_t0=(p1['t0']-180, p1['t0']+180),
+# 		   errordef=1,
+# 		   print_level=1
+# 		   )
 
-cid = fig.canvas.mpl_connect('button_press_event', onclick)
-plt.show()
-# print(m.values)
-cnopa = microlens_simple(t, 19., 0., m.values['u0'], m.values['t0'], m.values['tE'], 0., 0.)
+res=[]
+st1 = time.time()
+res.append(scipy.optimize.differential_evolution(fitter_minmax,
+			bounds=[(0, 1), (p1['t0'] - abs(p1['tE']), p1['t0'] + abs(p1['tE'])), (p1['tE'] * (1 - np.sign(p1['tE']) * 0.5), p1['tE'] * (1 + np.sign(p1['tE']) * 0.5))],
+			   disp=False, popsize=10, mutation=(0.5, 1.0), strategy='currenttobest1bin', atol=0.0001, recombination=0.9))
+res = res[0]
+print(time.time()-st1)
+print(res.fun)
+
+# def onclick(event):
+# 	m.migrad()
+
+# cid = fig.canvas.mpl_connect('button_press_event', onclick)
+# plt.show()
+
+# cnopa2 = microlens_simple(t, 19., 0., res.x[0], res.x[1], res.x[2], 0., 0.)
+cnopa2 = microlens_simple(t, 19., 0., m.values['u0'], m.values['t0'], m.values['tE'], 0., 0.)
 
 from mpl_toolkits.mplot3d import Axes3D
 
-explored_parameters = np.array(explored_parameters)
-print(explored_parameters.shape)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(explored_parameters[:,0], explored_parameters[:,1], explored_parameters[:,3])
-plt.show()
+# explored_parameters = np.array(explored_parameters)
+# print(explored_parameters.shape)
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(explored_parameters[:,0], explored_parameters[:,1], explored_parameters[:,3])
+# plt.show()
 # nrom1 = LogNorm(vmin=abs(explored_parameters[:,3].max()), vmax=0.1)
 # fig, axs= plt.subplots(2, 2, sharex='col', sharey='row')
 # axs[1, 0].scatter(explored_parameters[:,0], explored_parameters[:,1], c=np.abs(explored_parameters[:,3]), norm=nrom1)
@@ -237,18 +256,14 @@ plt.show()
 # axs[1, 1].scatter(explored_parameters[:,2], explored_parameters[:,1], c=np.abs(explored_parameters[:,3]), norm=nrom1)
 # plt.show()
 
-# nopa_center = numba_weighted_mean(t, 19 - cnopa)
-# para_center = numba_weighted_mean(t, 19 - cpara)
-# shift = nopa_center - para_center
-# print(shift)
-# cpara = microlens_parallax(t+shift, **p1)
 
 fig, axs = plt.subplots(nrows=2, ncols=1, sharex='col')
-axs[0].plot(t, cnopa)
-axs[0].plot(t, cpara)
+axs[0].plot(t, cpara, label='sun')
+axs[0].plot(t, cnopa2, label='corrected earth')
+axs[0].plot(t, cnopa, label='original earth')
+axs[0].legend()
 axs[0].invert_yaxis()
-axs[1].plot(t, np.abs(cnopa-cpara))
-axs[1].invert_yaxis()
+axs[1].plot(t, np.abs(cnopa2-cpara))
 plt.figure()
 nb_bins=100
 range_tE = (0, 2000)
