ad72f8d56c3e359ddc354c67567618f4ee124b1f
tristanblaineau <tristan.blaineau@orange.fr>
2019-05-09 14:29:53 +0200

    modified simple maxdiff computation and added full differential evolution minmax function

--- merger/parallax_estimator/similarity_estimator.py
+++ merger/parallax_estimator/similarity_estimator.py
@@ -5,14 +5,28 @@ from iminuit import Minuit
 import numba as nb
 import time
 
-from merger.old.parameter_generator import microlens_parallax, microlens_simple, generate_parameters
+import matplotlib.pyplot as plt
+from merger.old.parameter_generator import microlens_parallax, microlens_simple, generate_parameters, delta_u_from_x, tE_from_xvt
 from scipy.signal import find_peaks
 
-def distance1(cnopa, cpara):
-	return np.max(np.abs(cnopa-cpara))
+def distance1(t, params):
+	return np.max(np.abs(microlens_simple(t, **params)-microlens_parallax(t, **params)))
 
 def distance2(cnopa, cpara):
 	return np.abs(cnopa-cpara).sum()/np.sum(19.-cnopa)
+
+def simplemax_distance(t, params):
+	t = np.linspace(params['t0'] - 200, params['t0'] + 200, len(t))
+	return np.max(np.abs(np.max(np.abs(microlens_simple(t, **params)-microlens_parallax(t, **params)))))
+
+def max_fitter(t, params):
+	return -np.abs((microlens_parallax(t, 19, 0, params['u0'], params['t0'], params['tE'], params['delta_u'], params['theta']) - microlens_simple(t, 19., 0., params['u0'], params['t0'], params['tE'], 0., 0.)))
+
+def fit_simplemax_distance(t, params):
+	res = scipy.optimize.differential_evolution(max_fitter, bounds=[(params['t0'] - 400, params['t0'] + 400)], disp=False, popsize=40, mutation=(0.5, 1.0),
+	strategy='best1bin')
+	return res.fun
+
 
 # @nb.jit(nopython=True)
 # def dtw_distance(cnopa, cpara):
@@ -51,14 +65,16 @@ def distance2(cnopa, cpara):
 # 	# plt.show()
 # 	return distance
 
-def peak_distance(cnopa, cpara, min_prominence=0., base_mag=19.):
+def peak_distance(t ,params, min_prominence=0., base_mag=19.):
+	cnopa = microlens_simple(t, **params)
+	cpara = microlens_parallax(t, **params)
 	peaks, infos = find_peaks(cpara-base_mag, prominence=min_prominence)
 	if len(peaks):
 		return len(peaks)#np.max(infos["prominences"])
 	else :
 		return 0
 
-def scipy_simple_fit_distance(cnopa, cpara, time_range, init_params):
+def scipy_simple_fit_distance(t, init_params):
 	def fitter_func(params):
 		u0, t0, tE = params
 		return np.max(np.abs((cpara - microlens_simple(time_range, 19., 0., u0, t0, tE, 0., 0.))))
@@ -69,7 +85,7 @@ def scipy_simple_fit_distance(cnopa, cpara, time_range, init_params):
 def max_fitter(t, u0, t0, tE, pu0, pt0, ptE, pdu, ptheta):
 	return -np.abs((microlens_parallax(t, 19, 0, pu0, pt0, ptE, pdu, ptheta) - microlens_simple(t, 19., 0., u0, t0, tE, 0., 0.)))
 
-def fit_minmax_distance(cnopa, cpara, time_range, init_params):
+def minmax_distance_minuit(cnopa, cpara, time_range, init_params):
 	def fitter_minmax(u0, t0, tE):
 		return - scipy.optimize.differential_evolution(max_fitter, bounds=[(init_params['t0']-400, init_params['t0']+400)], args=(u0, t0, tE, init_params['u0'], init_params['t0'], init_params['tE'], init_params['delta_u'], init_params['theta']), disp=False, popsize=40, mutation=(0.5, 1.0)).fun
 	m = Minuit(fitter_minmax,
@@ -87,6 +103,15 @@ def fit_minmax_distance(cnopa, cpara, time_range, init_params):
 			   )
 	m.migrad()
 	return m.get_fmin()
+
+def minmax_distance_scipy(t, params):
+	def fitter_minmax(g):
+		u0, t0, tE = g
+		return - scipy.optimize.differential_evolution(max_fitter, bounds=[(params['t0']-400, params['t0']+400)], args=(u0, t0, tE, params['u0'], params['t0'], params['tE'], params['delta_u'], params['theta']),
+					disp=False, popsize=40, mutation=(0.5, 1.0)).fun
+	return scipy.optimize.differential_evolution(fitter_minmax, bounds=[(0, 1),
+				(params['t0'] - abs(params['tE']), params['t0'] + abs(params['tE'])), (params['tE'] * (1 - np.sign(params['tE']) * 0.5), params['tE'] * (1 + np.sign(params['tE']) * 0.5))],
+				disp=False, popsize=10, mutation=(0.5, 1.0), strategy='currenttobest1bin', atol=0.0001, recombination=0.9).fun
 
 @nb.njit
 def numba_weighted_mean(a, w):
@@ -112,15 +137,7 @@ def compute_distance(params_set, distance, time_sampling=1000):
 		del params['vt']
 		params['mag']=19.
 		params['blend']=0.
-		distance_args = {'time_range': t, 'init_params':{'u0':params['u0'], 'tE':params['tE'], 't0':params['t0'], 'delta_u':params['delta_u'], 'theta':params['theta']}}
-		# cnopa = microlens_simple(t, **params)
-		# cpara = microlens_parallax(t, **params)
-		# nopa_center = numba_weighted_mean(t, 19 - cnopa)
-		# para_center = numba_weighted_mean(t, 19 - cpara)
-		# shift = nopa_center - para_center
-		st1 = time.time()
-		ds.append(distance(microlens_simple(t, **params), microlens_parallax(t, **params), **distance_args))
-		print(time.time()-st1)
+		ds.append(distance(t, params))
 	return ds
 
 
@@ -135,7 +152,6 @@ def compute_distances(output_name, distance, mass, nb_samples, seed=1234567890):
 		all_params.append(generate_parameters(mass=mass, x=g[0], vt=g[1]))
 	df = pd.DataFrame.from_records(all_params)
 
-
 	st1 = time.time()
 	ds = compute_distance(all_params, distance=distance, time_sampling=1000)
 	print(time.time()-st1)
@@ -143,27 +159,27 @@ def compute_distances(output_name, distance, mass, nb_samples, seed=1234567890):
 	df = df.assign(distance=ds)
 	df.to_pickle(output_name)
 
-# all_params=[]
-# np.random.seed(1234567890)
-# all_xvts = np.load('../test/xvt_samples.npy')
-# idx = np.arange(0, len(all_xvts)-1)
-# np.random.shuffle(idx)
-# print(all_xvts[idx[0]])
-# all_xvts = all_xvts[idx]
-# plt.hist2d(delta_u_from_x(all_xvts[:,0], mass=60.), tE_from_xvt(all_xvts[:,0], all_xvts[:,1], mass=60.), bins=300, range=((0, 0.05), (0, 1000)))
-# plt.show()
-# all_xvts = all_xvts[:10]
-# plt.hist2d(delta_u_from_x(all_xvts[:,0], mass=60.), tE_from_xvt(all_xvts[:,0], all_xvts[:,1], mass=60.), bins=300, range=((0, 0.05), (0, 1000)))
-# plt.show()
-# for mass in np.geomspace(0.1, 1000, 5):
-# 	for g in all_xvts:
-# 		all_params.append(generate_parameters(mass=mass, x=g[0], vt=g[1]))
-# df = pd.DataFrame.from_records(all_params)
-#
-#
-# st1 = time.time()
-# ds = compute_distance(all_params, distance=fit_minmax_distance, time_sampling=1000)
-# print(time.time()-st1)
-#
-# df = df.assign(distance=ds)
-# df.to_pickle('temp_fittermax.pkl')
+all_params=[]
+np.random.seed(1234567890)
+all_xvts = np.load('../test/xvt_samples.npy')
+idx = np.arange(0, len(all_xvts)-1)
+np.random.shuffle(idx)
+print(all_xvts[idx[0]])
+all_xvts = all_xvts[idx]
+plt.hist2d(delta_u_from_x(all_xvts[:,0], mass=60.), tE_from_xvt(all_xvts[:,0], all_xvts[:,1], mass=60.), bins=300, range=((0, 0.05), (0, 1000)))
+plt.show()
+all_xvts = all_xvts[:1]
+plt.hist2d(delta_u_from_x(all_xvts[:,0], mass=60.), tE_from_xvt(all_xvts[:,0], all_xvts[:,1], mass=60.), bins=300, range=((0, 0.05), (0, 1000)))
+plt.show()
+for mass in np.geomspace(0.1, 1000, 5):
+	for g in all_xvts:
+		all_params.append(generate_parameters(mass=mass, x=g[0], vt=g[1]))
+df = pd.DataFrame.from_records(all_params)
+
+
+st1 = time.time()
+ds = compute_distance(all_params, distance=minmax_distance_scipy, time_sampling=1000)
+print(time.time()-st1)
+
+df = df.assign(distance=ds)
+df.to_pickle('temp_fittest.pkl')
