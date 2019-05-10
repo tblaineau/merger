7a29d0c3ad316ee4862baf6d298400d659e4bb77
tristanblaineau <tristan.blaineau@orange.fr>
2019-05-10 11:09:59 +0200

    added fuse script for job outputs

---
+++ merger/parallax_estimator/fittermax/fuse_outputs.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+fs = []
+for (_, _, filenames) in os.walk("."):
+	for file in filenames:
+		print(file[-3:])
+		if file[-3:] == "pkl":
+			fs.append(pd.read_pickle(file))
+
+all = pd.concat(fs)
+pd.to_pickle(all, 'scipyminmax1000.pkl')
