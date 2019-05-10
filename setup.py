380acb72e3390373dcae596f4e9e1a9535358396
tristanblaineau <tristan.blaineau@orange.fr>
2019-05-10 14:38:51 +0200

    updated required python modules

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 from setuptools import setup, find_packages
 
-setup(name='merger', version='0.0a', packages=find_packages())
+setup(name='merger',
+	  version='0.0a',
+	  packages=find_packages(),
+	  install_requires=[
+		  'numpy',
+		  'pandas',
+		  'iminuit',
+		  'scipy',
+		  'matplotlib',
+		  'astropy',
+		  'numba',
+		  'sklearn'
+	  ]
+	  )
