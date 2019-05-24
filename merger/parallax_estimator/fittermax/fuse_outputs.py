722f928f6ab09c21f9fce44aeb421801cea0f9cf
tristanblaineau <tristan.blaineau@orange.fr>
2019-05-24 16:44:05 +0200

    changed filenames, path+

--- merger/parallax_estimator/fittermax/fuse_outputs.py
+++ merger/parallax_estimator/fittermax/fuse_outputs.py
@@ -2,12 +2,12 @@ import pandas as pd
 import os
 
 fs = []
-for (_, _, filenames) in os.walk("."):
+for (root, dir, filenames) in os.walk("./6M12"):
 	for file in filenames:
 		print(file[-3:])
 		if file[-3:] == "pkl":
-			fs.append(pd.read_pickle(file))
+			fs.append(pd.read_pickle(os.path.join(root, file)))
 
 all = pd.concat(fs)
 all.reset_index(inplace=True)
-pd.to_pickle(all, 'fastscipyminmax6M.pkl')
+pd.to_pickle(all, 'fastscipyminmax6M12.pkl')
