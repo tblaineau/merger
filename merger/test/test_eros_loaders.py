d127fc8fc0422e07827a261e652ab410dd27a924
tristanblaineau <tristan.blaineau@orange.fr>
2019-06-11 10:21:37 +0200

    name chaned

---
+++ merger/test/test_eros_loaders.py
@@ -0,0 +1,17 @@
+import os, time
+
+import merger.old.merger_library as mrgl
+
+#Load test EROS
+
+#Field lm0103n
+INPUT_PATH = '/Volumes/DisqueSauvegarde/EROS/lightcurves'
+
+st1 = time.time()
+t1 = mrgl.load_eros_compressed_files(os.path.join(INPUT_PATH, 'lm/lm010/lm0103n-lc.tar.gz'))
+st2 = time.time()
+t2 = mrgl.load_eros_files(os.path.join(INPUT_PATH, 'lm_ex/lm010/lm0103/lm0103n'))
+st3 = time.time()
+
+print(f'Compressed reading time : {st2-st1} seconds for {len(t1)} lines.')
+print(f'.time reading time : {st3-st2} seconds for {len(t2)} lines')
