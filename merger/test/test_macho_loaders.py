da6dce150bbb3057b0dcaf37c95d3e2386056f47
tristanblaineau <tristan.blaineau@orange.fr>
2019-06-11 11:36:38 +0200

    full test macho

--- merger/test/test_macho_loaders.py
+++ merger/test/test_macho_loaders.py
@@ -1,8 +1,16 @@
 import merger.clean.libraries.merger_library as mrgl
-import time
+import os, time
+
+INPUT_PATH = '/Volumes/DisqueSauvegarde/MACHO/lightcurves'
 
 def test_loading_macho(url=False):
 	st1 = time.time()
-	mrgl.load
+	t1 = mrgl.read_macho_lightcurve(os.path.join(INPUT_PATH, 'F_1'),'F_1.3319.gz')
+	st2 = time.time()
 	if url:
-		mrgl.load_macho_from_url('F_1.3319.gz')
+		t2 = mrgl.load_macho_from_url('F_1.3319.gz')
+		st3 = time.time()
+
+	print(f'Compressed reading time : {st2-st1} seconds for {len(t1)} lines.')
+	if url:
+		print(f'url reading time : {st3-st2} seconds for {len(t2)} lines.')
