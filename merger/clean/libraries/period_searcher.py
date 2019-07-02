b6cd7f5339d081df8c34e8e1eda8280bc6f0655d
tristanblaineau <tristan.blaineau@orange.fr>
2019-07-02 10:28:24 +0200

    add file for period_search/validation

---
+++ merger/clean/libraries/period_searcher.py
@@ -0,0 +1,63 @@
+import numba as nb
+import numpy as np
+import scipy.signal
+import scipy.special
+
+
+@nb.njit
+def numba_mean(values):
+	s = 0
+	for i in range(len(values)):
+		s+=values[i]
+	return s/len(values)
+
+
+@nb.njit
+def chi2_int(phase, mag, err):
+	chi2 = 0
+	lt = len(phase)
+	for i in range(0, lt - 2):
+		if (phase[i + 2] - phase[i]) == 0:
+			ri = 0.5
+		else:
+			ri = (phase[i + 1] - phase[i]) / (phase[i + 2] - phase[i])
+		sigmaisq = err[i + 1] ** 2 + (1 - ri) ** 2 * err[i] ** 2 + ri ** 2 * err[i + 2] ** 2
+		chi2 += (mag[i + 1] - mag[i] - ri * (mag[i + 2] - mag[i])) ** 2 / sigmaisq
+
+	ri = (phase[lt - 1] - phase[lt - 2]) / (phase[0] - phase[lt - 2])
+	sigmaisq = err[lt - 1] ** 2 + (1 - ri) ** 2 * err[lt - 2] ** 2 + ri ** 2 * err[0] ** 2
+	chi2 += (mag[lt - 1] - mag[lt - 2] - ri * (mag[0] - mag[lt - 2])) ** 2 / sigmaisq
+	return chi2
+
+
+@nb.njit
+def period_search_loop(time, mag, err, frequency):
+	phase=[]
+	for i in range(len(time)):
+		phase.append(time[i]%(1/frequency))
+	phase = np.array(phase)
+	idx = np.argsort(phase)
+	return chi2_int(phase[idx], mag[idx], err[idx])
+
+
+@nb.njit
+def auto_correlation(time, mag, err, min_freq, step_freq, nb_steps):
+	chi2s = []
+	freqs = []
+	for i in range(nb_steps):
+		chi2s.append(period_search_loop(time, mag, err, min_freq+i*step_freq))
+		freqs.append(min_freq+i*step_freq)
+	return chi2s, freqs
+
+
+def confidence(time, mag, err, min_freq, step_freq, nb_steps):
+	chi2s, freqs = auto_correlation(time, mag, err, min_freq, step_freq, nb_steps)
+	freqs = np.array(freqs)
+	mean_chi2 = np.mean(chi2s)
+	reduced_chi2s = ((chi2s - mean_chi2) / mean_chi2) * np.sqrt(len(time) / 2.)
+	min_chi2 = reduced_chi2s.min()
+	if mean_chi2 <= 0:
+		best_proba = 1.
+	else:
+		best_proba = np.log10(nb_steps / 2. * scipy.special.erfc(-min_chi2 / np.sqrt(2)))
+	return reduced_chi2s, freqs, best_proba, min_chi2
