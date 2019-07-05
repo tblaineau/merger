10970480ebf5dad5c880a4b26a432160b3eaf07d
tristanblaineau <tristan.blaineau@orange.fr>
2019-07-05 17:27:59 +0200

    added star count function

--- merger/utils/line_count.py
+++ merger/utils/line_count.py
@@ -35,6 +35,21 @@ def count_all(filepath):
 	print(time.time()-st1)
 	return np.array(alls, dtype=[('tile', 'i4'), ('linecount', 'i4')])
 
+def star_count(filepath):
+	st1 = time.time()
+	alls = []
+	for filename in os.listdir(filepath):
+		print(filename)
+		if filename[-3:] == '.gz':
+			alls.append((int(filename.split('.')[1]),
+						 len(
+							 np.unique(
+							 	np.loadtxt(os.path.join(filepath, filename), delimiter=';', usecols=3)
+							 ))
+						 ))
+	print(time.time()-st1)
+	return np.array(alls, dtype=[('tile', 'i4'), ('starcount', 'i4')])
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--path', type=str, required=True, help="Path to MACHO lightcurves files.")
@@ -45,8 +60,8 @@ if __name__ == '__main__':
 	MACHO_files_path = args.path
 	output_path = args.output_path
 
-	np.savetxt(os.path.join(output_path,'lncnt_'+str(42)+'.txt'), X=count_all(os.path.join(MACHO_files_path, 'F_' + str(42))), fmt='%d')
+	np.savetxt(os.path.join(output_path,'strcnt_'+str(42)+'.txt'), X=star_count(os.path.join(MACHO_files_path, 'F_' + str(42))), fmt='%d')
 
 	for field in range(1, 83):
 		print(field)
-		np.savetxt(os.path.join(output_path,'lncnt_' + str(field)+'.txt'), X=count_all(os.path.join(MACHO_files_path, 'F_'+str(field))), fmt='%d')
+		np.savetxt(os.path.join(output_path,'strcnt_' + str(field)+'.txt'), X=star_count(os.path.join(MACHO_files_path, 'F_'+str(field))), fmt='%d')
