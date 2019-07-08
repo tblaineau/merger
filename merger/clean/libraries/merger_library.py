00f138c07348c7a8cc76b4f07c24e625c1f3f8bc
tristanblaineau <tristan.blaineau@orange.fr>
2019-07-08 16:09:32 +0200

    too much time to load error

--- merger/clean/libraries/merger_library.py
+++ merger/clean/libraries/merger_library.py
@@ -510,6 +510,8 @@ def merger_macho_first(output_dir_path, MACHO_field, EROS_files_path, correspond
 							lc["id_E"].append(id_E)
 					pds.append(pd.DataFrame.from_dict(lc))
 					logging.info(time.time() - st2)
+					if time.time() - st2 > 2:
+						raise SystemExit(f"EROS loading take too much time. No time to waste.")
 		eros_lcs = pd.concat(pds)
 		del pds
 		logging.info(f"{time.time()-st1} seconds to load {eros_lcs.id_E.nunique()}.")
