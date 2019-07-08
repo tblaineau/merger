d0151cf5646736b2d2000da432d42cb25bf11aec
tristanblaineau <tristan.blaineau@orange.fr>
2019-07-08 10:42:09 +0200

    add package data file (star counts)

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(name='merger',
-	  version='0.0a',
-	  packages=find_packages(),
-	  install_requires=[
-		  'numpy',
-		  'pandas',
-		  'iminuit',
-		  'scipy',
-		  'matplotlib',
-		  'astropy',
-		  'numba',
-		  'sklearn'
-	  ]
-	  )
+	version='0.0a',
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=[
+		'numpy',
+		'pandas',
+		'iminuit',
+		'scipy',
+		'matplotlib',
+		'astropy',
+		'numba',
+		'sklearn']
+	)
