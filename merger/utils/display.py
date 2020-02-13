30bb15aadd803979d63c9c362ac8912651fd6a01
tristanblaineau <tristan.blaineau@orange.fr>
2020-02-13 10:07:58 +0100

    correct range

--- merger/utils/display.py
+++ merger/utils/display.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import logging
 
-def hist2d1d(x, y, bins=10, figsize=None, hist_args={"histtype":"step"}, **kwargs):
+def hist2d1d(x, y, bins=10, figsize=None, hist_args={"histtype":"step"}, range=range, **kwargs):
 	fig, ax = plt.subplots(nrows=2, ncols=2, sharex="col", sharey="row", figsize=figsize, gridspec_kw={'width_ratios': [3, 1], 'height_ratios': [1, 3]})
-	ax[1, 0].hist2d(x, y, bins=bins, **kwargs)
+	ax[1, 0].hist2d(x, y, bins=bins, range=range, **kwargs)
 	try:
 		if not isinstance(bins, int):
 			binx = bins[0]
@@ -13,6 +13,6 @@ def hist2d1d(x, y, bins=10, figsize=None, hist_args={"histtype":"step"}, **kwarg
 			biny = bins
 	except ValueError:
 		logging.error(f"Bad value for bins, expected int or sequence(int, int) : {bins}")
-	ax[0, 0].hist(x, bins=binx, **hist_args)
-	ax[1, 1].hist(y, bins=biny, orientation='horizontal', **hist_args)
+	ax[0, 0].hist(x[(y>range[1][0]) & (y<range[1][1])], bins=binx, range=range[0], **hist_args)
+	ax[1, 1].hist(y[(x>range[0][0]) & (x<range[0][1])], bins=biny, range=range[1], orientation='horizontal', **hist_args)
 	ax[0, 1].axis('off')
