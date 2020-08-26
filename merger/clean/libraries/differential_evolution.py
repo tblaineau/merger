7c7f1c64d3270a86a945b508f3ff9958a5c2c9c7
tristanblaineau <tristan.blaineau@orange.fr>
2020-08-26 15:06:55 +0200

    removed hesse exception (that never catch)

--- merger/clean/libraries/differential_evolution.py
+++ merger/clean/libraries/differential_evolution.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numba as nb
 from merger.clean.libraries.merger_library import COLOR_FILTERS
-from iminuit import Minuit, iminuit_warnings
+from iminuit import Minuit
 import logging
 import pandas as pd
 from sklearn.utils.random import sample_without_replacement
@@ -454,17 +454,13 @@ def fit_ml_de_simple(subdf, do_cut5=False, hesse=False, minos=False):
 	if hesse:
 		micro_error_labels = ["error_" + name for name in micro_keys]
 		micro_errors = [np.nan]*len(micro_keys)
-		try:
-			m_micro.hesse()
-		except iminuit_warnings.HesseFailedWarning:
-			logging.warning("Hesse failed on star " + str(subdf.name))
-		else:
-			micro_errors = [m_micro.errors["u0"], m_micro.errors["t0"], m_micro.errors["tE"]]
-			for key in COLOR_FILTERS.keys():
-				if key in ufilters:
-					micro_errors.append(m_micro.errors["magStar_" + key])
-				else:
-					micro_errors.append(np.nan)
+		m_micro.hesse()
+		micro_errors = [m_micro.errors["u0"], m_micro.errors["t0"], m_micro.errors["tE"]]
+		for key in COLOR_FILTERS.keys():
+			if key in ufilters:
+				micro_errors.append(m_micro.errors["magStar_" + key])
+			else:
+				micro_errors.append(np.nan)
 	elif minos:
 		for name in micro_keys:
 			micro_error_labels+=["lower_error_"+name, "upper_error_"+name, "valid_lower_error_"+name, "valid_upper_error_"+name]
