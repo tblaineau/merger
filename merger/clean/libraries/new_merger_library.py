0facbd66ff6d058d3b4270c7eabafb74d5f16b38
tristanblaineau <tristan.blaineau@orange.fr>
2021-04-22 14:36:02 +0200

    added function for smalle sample

--- merger/clean/libraries/new_merger_library.py
+++ merger/clean/libraries/new_merger_library.py
@@ -48,6 +48,152 @@ def h(xi, xj, xm, p):
 	else:
 		return ((xi - xm) - (xm - xj))/(xi - xj)
 
+
+def merger_small_sample(output_dir_path, start, end,
+					  correspondance_file_path = "/pbs/home/b/blaineau/work/notebooks/combined.parquet",
+					  macho_files_path="/sps/eros/users/blaineau/small_sample/macho/sample_macho.parquet",
+					  eros_files_path = "/sps/eros/users/blaineau/small_sample/eros/sample_eros.parquet",
+					  eros_ratio_path = "/pbs/home/b/blaineau/work/notebooks/eros_cleaning/ratios",
+					  macho_ratio_path = "/pbs/home/b/blaineau/work/bad_times/bt_macho",
+					  save=True,
+					  verbose=False):
+	if verbose:
+		logging.basicConfig(level=logging.INFO)
+
+	st1 = time.time()
+	# Load combined ids
+	logging.info("Reading correspondance file.")
+	ids = pd.read_parquet(correspondance_file_path, columns=["id_M", "id_E"])
+	sample_ids = np.loadtxt("/pbs/home/b/blaineau/work/simulation_prod/sample.csv", dtype=str)
+	sample_ids = pd.DataFrame(sample_ids, columns=["id_E", "id_M"])
+	ids = ids[(ids.id_E.isin(sample_ids.id_E) | ids.id_M.isin(sample_ids.id_M))]
+
+	id_Es = ids.id_E
+
+	logging.info("Loading EROS light curves")
+	quarts = id_Es.str[:7].unique()
+	keep_E = []
+	for quart in quarts:
+		logging.info(quart)
+		try:
+			path = os.path.join(eros_files_path, quart[:5], quart + ".parquet")
+		except TypeError:
+			continue
+		d = pd.read_parquet(path)
+		keep_E.append(d[d.id_E.isin(id_Es)])
+	del d
+	keep_E = pd.concat(keep_E)
+	keep_E.time += 49999.5
+
+	# Clean EROS
+	logging.info("Cleaning EROS light curves")
+	ccds = keep_E.id_E.str[:6]
+	# r=0, b=1
+	blue_eros_max_ratio = 0.015
+	red_eros_max_ratio = 0.017
+	for ccd in ccds.unique():
+		if ccd is None:
+			continue
+		ratios = pd.read_parquet(os.path.join(eros_ratio_path, "ratios_" + ccd + ".parquet"))
+		for color_ratio, color, color_err in zip(["high_distance_b10", "high_distance_r10"], ["red_E", "blue_E"],
+												 ["rederr_E", "blueerr_E"]):
+			g = ratios[color_ratio][ratios[color_ratio] > 0]
+			x = g.values
+			a, xm, b = np.nanquantile(x, q=[0.25, 0.5, 0.75])
+			p = len(x[x <= xm])
+			hs = []
+			for xj in x[x <= xm]:
+				for xi in x[x > xm]:
+					hs.append(h(xi, xj, xm, p))
+			mc = np.median(hs)
+			if mc < 0:
+				# mc=0
+				w3 = b + 1.5 * np.exp(4 * mc) * (b - a)
+			else:
+				w3 = b + 1.5 * np.exp(3 * mc) * (b - a)
+
+			r = g[(g > w3) | (g > red_eros_max_ratio)].sort_values(ascending=False)
+			one_percent = int(np.round(0.01 * len(g)))
+			if len(r) > one_percent:
+				g = g.iloc[:one_percent]
+			keep_E.loc[keep_E["time"].isin(r.index), [color, color_err]] = np.nan
+
+	# Load and remove JBM times
+	"""discard = pd.read_csv("/pbs/home/b/blaineau/work/notebooks/eros_cleaning/discard.txt", sep="/", usecols=[0, 1, 2, 3, 5], names=["target", "n_ccd", "n_color", "n_quart", "name"])
+	discard["n_time"] = discard["name"].str[10:-5]
+	times_translation = pd.read_csv("datesall/all.csv", usecols=[0, 1], names=["n_time", "hjd"])
+	discard = pd.merge(discard, times_translation, on="n_time")"""
+
+	# droping empty lines
+	keep_E.dropna(subset=["red_E", "blue_E"], how="all", inplace=True)
+	keep_E = pd.merge(keep_E, ids, on="id_E", how="left")
+
+	# LOADING MACHO
+	logging.info("Loading MACHO light curves.")
+	splitted = ids.id_M.str.split(":", expand=True)
+	tiles = splitted[0].str.cat(splitted[1], "_").unique()
+	del splitted
+	keep_M = []
+	for tile in tiles:
+		logging.info("   loading tile : " + str(tile))
+		try:
+			# path = os.path.join("/sps/eros/users/blaineau/macho_fast_read/",
+			#                    "F_"+tile.split("_")[0], "F_"+tile+".parquet",
+			#                   )
+			path = os.path.join(macho_files_path + tile.split("_")[0],
+								"F_" + tile.replace("_", ".") + ".gz")
+		except TypeError:
+			continue
+		d = pd.read_csv(path,
+						sep=";", usecols=[1, 2, 3, 4, 9, 10, 17, 24, 25, 32],
+						names=["field", "tile", "starid", "time", "red_M", "rederr_M", "red_amp", "blue_M", "blueerr_M",
+							   "blue_amp"]
+						)
+		d.loc[:, "id_M"] = d.field.astype(str).str.cat([d.tile.astype(str), d.starid.astype(str)], sep=":")
+		d = d.drop(["field", "tile", "starid"], axis=1)
+		# d = pd.read_parquet(path)#, columns=["time", "red_M", "rederr_M", "blue_M", "blueerr_M", "id_M"])
+		keep_M.append(d[d.id_M.isin(ids.id_M)])
+	del d
+	keep_M = pd.concat(keep_M)
+
+	# Cleaning MACHO lcs.
+	max_macho_fraction = 0.05
+	logging.info("Cleaning MACHO light curves")
+	fields = keep_M.id_M.str.split(":").str[0].unique()
+	for field in fields:
+		dfb = pd.DataFrame(np.load(os.path.join(macho_ratio_path, str(field) + "_blue_M_ratios.npy")),
+						   columns=["blue_amp", "time", "ratio"])
+		pms = list(zip(keep_M["time"].values, keep_M["blue_amp"].values))
+		pdf = list(zip(dfb[dfb.ratio > 0.05]["time"].values, dfb[dfb.ratio > 0.05]["blue_amp"].values))
+		result = pd.Series(pms).isin(pdf)
+		keep_M.loc[result].blue_M = np.nan
+		keep_M.loc[result].blueerr_M = np.nan
+
+		dfr = pd.DataFrame(np.load(os.path.join(macho_ratio_path, str(field) + "_red_M_ratios.npy")),
+						   columns=["red_amp", "time", "ratio"])
+		pms = list(zip(keep_M["time"].values, keep_M["red_amp"].values))
+		pdf = list(zip(dfr[dfr.ratio > 0.05]["time"].values, dfr[dfr.ratio > 0.05]["red_amp"].values))
+		result = pd.Series(pms).isin(pdf)
+		keep_M.loc[result].red_M = np.nan
+		keep_M.loc[result].rederr_M = np.nan
+
+	keep_M = pd.merge(keep_M, ids, on="id_M", how="left", validate="m:1")
+
+	# MERGING LIGHT CURVES
+	logging.info("Merging light curves...")
+	merged = pd.concat([keep_E, keep_M])
+	del keep_E
+	del keep_M
+
+	# save merged dataframe
+	if save:
+		logging.info("Saving")
+		merged.to_parquet(os.path.join(output_dir_path, str(start) + "_" + str(end) + ".parquet"), index=False)
+
+	logging.info("Total merging time : " + str(time.time() - st1))
+	return merged
+
+
 from memory_profiler import profile
 
 @profile
