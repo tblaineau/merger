fd9dec57638583e7c455cc619896759d0515f82e
tristanblaineau <tristan.blaineau@orange.fr>
2021-06-17 18:19:05 +0200

    update to compute on CCD quart rather than chunks

--- merger/utils/gaia_associator.py
+++ merger/utils/gaia_associator.py
@@ -95,93 +95,134 @@ temp_gaia = gaia_coord[sep<distance]
 
 corrected = []
 factors = []
-pc = np.array(macho.drop_duplicates(["pier", "chunk"])[["pier", "chunk"]])
-corrected = []
-factors = []
 
-for p in pc:
-	c_macho = macho[(macho.pier == p[0]) & (macho.chunk == p[1])]
-	c_temp_macho = c_macho.iloc[np.random.randint(0, len(c_macho), 1500)]
-	print(p)
-	if int(p[1]) == 255:
-		corrected.append(np.append(c_macho.id_M[:, None], c_macho[["ra", "dec"]].values, axis=1))
-		factors.append([0.] * 6)
-		continue
-	c_macho_coord = SkyCoord(c_macho.ra.values, c_macho.dec.values, unit=u.rad)
-	c_temp_macho_coord = SkyCoord(c_temp_macho.ra.values, c_temp_macho.dec.values, unit=u.rad)
-	temp_corrected = None
-	temp_factors = None
-	offra = (c_macho.ra.max() - c_macho.ra.min())
-	offdec = (c_macho.dec.max() - c_macho.dec.min())
+quartpc_w = [
+	[120, 121, 124, 125],
+	[0, 1, 4, 5],
+	[122, 123, 126, 127],
+	[2, 3, 6, 7],
+	[10, 11, 14, 15],
+	[8, 9, 12, 13],
+	[18, 19, 22, 23],
+	[16, 17, 20, 21],
+	[48, 49, 52, 53],
+	[50, 51, 54, 55],
+	[40, 41, 44, 45],
+	[42, 43, 46, 47],
+	[26, 27, 30, 31],
+	[24, 25, 28, 29],
+	[34, 35, 38, 39],
+	[32, 33, 36, 37]
+]
+quartpc_e = [
+	[108, 109, 104, 105],
+	[106, 107, 110, 111],
+	[112, 113, 116, 117],
+	[114, 115, 118, 119],
+	[96, 97, 100, 101],
+	[88, 89, 92, 93],
+	[98, 99, 102, 103],
+	[90, 91, 94, 95],
+	[58, 59, 62, 63],
+	[56, 57, 60, 61],
+	[66, 67, 70, 71],
+	[64, 65, 68, 69],
+	[74, 75, 78, 79],
+	[82, 83, 86, 87],
+	[72, 73, 76, 77],
+	[80, 81, 84, 85]
+]
 
-	c_temp_gaia = temp_gaia[(temp_gaia.ra.rad < c_macho.ra.max() + 1 * offra) &
-							(temp_gaia.ra.rad > c_macho.ra.min() - 1 * offra) &
-							(temp_gaia.dec.rad < c_macho.dec.max() + 1 * offdec) &
-							(temp_gaia.dec.rad > c_macho.dec.min() - 1 * offdec)
-							]
-	print(len(c_temp_gaia))
-	print(len(temp_gaia))
-	print(len(c_macho))
-	if len(c_temp_gaia) > 10000:
-		c_temp_gaia = c_temp_gaia[np.random.choice(len(c_temp_gaia), replace=False, size=10000)]
+quartpc = quartpc_w + quartpc_e+[[255]]
 
-	s2 = np.array(to_cartesian(np.array([c_temp_gaia.ra.rad, c_temp_gaia.dec.rad]).T))
-	k = KDTree(s2, metric="euclidean", leaf_size=30)
-
-
-	def minuit(x):
-		temp = transform(c_temp_macho_coord.ra.rad, c_temp_macho_coord.dec.rad, *x)
-		temp = np.array(to_cartesian(temp))
-		d3d = k.query(temp)[0].flatten()
-		v = 1 / np.sum(1 / (d3d * 180 / np.pi * 3600 + 0.1))
-		print(v, end="\r")
-		return v  # np.sum(d3d[c])/c.sum()
-
-
-	bounds = [(c_macho.ra.min() - 2 * offra, c_macho.ra.max() + 2 * offra),
-			  (c_macho.dec.min() - 2 * offdec, c_macho.dec.max() + 2 * offdec),
-			  (0.98, 1.02), (0.98, 1.02), (0, 2 * np.pi), (-5 * np.pi / 180., 5 * np.pi / 180.),
-			  (-2 * u.arcsec.to(u.rad), 2 * u.arcsec.to(u.rad)), (-2 * u.arcsec.to(u.rad), 2 * u.arcsec.to(u.rad))
-			  ]
-	i = 0
-	pop = 40
-	imax = 3
-	prec = 0
-	while i < imax:
-		print(i)
-		res = scipy.optimize.differential_evolution(minuit, bounds=bounds, popsize=pop, recombination=0.9,
-													mutation=(0.1, 0.3), strategy="randtobest1bin",
-													disp=True, maxiter=40)
-		print(res)
-		res = res.x
-		out = transform(c_macho_coord.ra.rad, c_macho_coord.dec.rad, *res)
-
-		correct_macho = SkyCoord(out[:, 0], out[:, 1], unit=u.rad)
-		i1, i2, d2d, _ = correct_macho.search_around_sky(c_temp_gaia, seplimit=2 * u.arcsec)
-		dra, ddec = correct_macho[i2].spherical_offsets_to(c_temp_gaia[i1])
-
-		tp = (d2d.arcsec < 0.2).sum() / (d2d.arcsec < 2).sum()
-		if prec < tp:
-			prec = tp
-			temp_corrected = out
-			temp_factors = res
-			print(prec)
-			pop = 20
-		if (d2d.arcsec < 0.5).sum() / (d2d.arcsec < 2).sum() > 0.85:
-			corrected.append(np.append(c_macho.id_M.values[:, None], out, axis=1))
-			factors.append(res)
-			i = 100
-			break
-		i += 1
-	if i == imax:
-		if not (temp_corrected is None):
-			corrected.append(np.append(c_macho.id_M.values[:, None], temp_corrected, axis=1))
-			factors.append(temp_factors)
-		else:
-			corrected.append(np.append(c_macho.id_M.values[:, None], c_macho[["ra", "dec"]].values, axis=1))
+j=0
+for pier in ["W", "E"]:
+	for chunks in quartpc:#pc:
+		print(" ")
+		print(pier, chunks)
+		c_macho = macho[(macho["pier"]==pier) & (macho["chunk"].isin(chunks))]
+		if len(c_macho)==0:
+			continue
+		c_temp_macho = c_macho.iloc[np.random.randint(0, len(c_macho), 5000)]
+		if chunks == [255]:
+			corrected.append(np.append(c_macho.id_M[:, None], c_macho[["ra", "dec"]].values, axis=1))
 			factors.append([0.] * 6)
-			print("Failed")
-	print("MACHO loaded")
+			continue
+		c_macho_coord = SkyCoord(c_macho.ra.values, c_macho.dec.values, unit=u.rad)
+		c_temp_macho_coord = SkyCoord(c_temp_macho.ra.values, c_temp_macho.dec.values, unit=u.rad)
+		temp_corrected = None
+		temp_factors = None
+		offra = (c_macho.ra.max() - c_macho.ra.min())
+		offdec = (c_macho.dec.max() - c_macho.dec.min())
+
+		c_temp_gaia = temp_gaia[(temp_gaia.ra.rad < c_macho.ra.max() + 1 * offra) &
+								(temp_gaia.ra.rad > c_macho.ra.min() - 1 * offra) &
+								(temp_gaia.dec.rad < c_macho.dec.max() + 1 * offdec) &
+								(temp_gaia.dec.rad > c_macho.dec.min() - 1 * offdec)
+								]
+		print(len(c_temp_gaia))
+		print(len(temp_gaia))
+		print(len(c_macho))
+		if len(c_temp_gaia) > 10000:
+			c_temp_gaia = c_temp_gaia[np.random.choice(len(c_temp_gaia), replace=False, size=10000)]
+
+		s2 = np.array(to_cartesian(np.array([c_temp_gaia.ra.rad, c_temp_gaia.dec.rad]).T))
+		k = KDTree(s2, metric="euclidean", leaf_size=30)
+
+
+		def minuit(x):
+			temp = transform(c_temp_macho_coord.ra.rad, c_temp_macho_coord.dec.rad, *x)
+			temp = np.array(to_cartesian(temp))
+			d3d = k.query(temp)[0].flatten()
+			v = 1 / np.sum(1 / (d3d * 180 / np.pi * 3600 + 0.1))
+			print(v, end="\r")
+			return v  # np.sum(d3d[c])/c.sum()
+
+
+		bounds = [(c_macho.ra.min() - 2 * offra, c_macho.ra.max() + 2 * offra),
+				  (c_macho.dec.min() - 2 * offdec, c_macho.dec.max() + 2 * offdec),
+				  (0.98, 1.02), (0.98, 1.02), (0, 2 * np.pi), (-5 * np.pi / 180., 5 * np.pi / 180.),
+				  (-2 * u.arcsec.to(u.rad), 2 * u.arcsec.to(u.rad)), (-2 * u.arcsec.to(u.rad), 2 * u.arcsec.to(u.rad))
+				  ]
+		i = 0
+		pop = 40
+		imax = 3
+		prec = 0
+		while i < imax:
+			print(i)
+			res = scipy.optimize.differential_evolution(minuit, bounds=bounds, popsize=pop, recombination=0.9,
+														mutation=(0.1, 0.3), strategy="randtobest1bin",
+														disp=True, maxiter=60)
+			print(res)
+			res = res.x
+			out = transform(c_macho_coord.ra.rad, c_macho_coord.dec.rad, *res)
+
+			correct_macho = SkyCoord(out[:, 0], out[:, 1], unit=u.rad)
+			i1, i2, d2d, _ = correct_macho.search_around_sky(c_temp_gaia, seplimit=2 * u.arcsec)
+			dra, ddec = correct_macho[i2].spherical_offsets_to(c_temp_gaia[i1])
+
+			tp = (d2d.arcsec < 0.2).sum() / (d2d.arcsec < 2).sum()
+			if prec < tp:
+				prec = tp
+				temp_corrected = out
+				temp_factors = res
+				print(prec)
+				pop = 20
+			if (d2d.arcsec < 0.5).sum() / (d2d.arcsec < 2).sum() > 0.85:
+				corrected.append(np.append(c_macho.id_M.values[:, None], out, axis=1))
+				factors.append(res)
+				i = 100
+				break
+			i += 1
+		if i == imax:
+			if not (temp_corrected is None):
+				corrected.append(np.append(c_macho.id_M.values[:, None], temp_corrected, axis=1))
+				factors.append(temp_factors)
+			else:
+				corrected.append(np.append(c_macho.id_M.values[:, None], c_macho[["ra", "dec"]].values, axis=1))
+				factors.append([0.] * 6)
+				print("Failed")
+		print("MACHO loaded")
 
 out_path = sys.argv[2]
 np.savetxt(os.path.join(out_path, "macho_" + str(field) + "_corrected.csv"), np.concatenate(corrected), delimiter=" ",fmt='%s')
